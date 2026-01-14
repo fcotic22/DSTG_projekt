a80624510fd7f14ca551d7c456f877ac3a09eec3
Franjo Čotić <fcotic22@student.foi.hr>
2026-01-14 11:07:04 +0100

    Dodana lista petlji za sve jezike

--- DSTG_projekt/MainWindow.xaml.cs
+++ DSTG_projekt/MainWindow.xaml.cs
@@ -1,94 +1,102 @@
-﻿using System.IO;
-using System.Windows;
-using System.Windows.Controls;
+﻿    using System.IO;
+    using System.Windows;
+    using System.Windows.Controls;
 
-namespace DSTG_projekt
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    namespace DSTG_projekt
     {
-        public MainWindow()
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+        public partial class MainWindow : Window
         {
-            InitializeComponent();
-        }
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
 
-        private string? Filepath { get; set; }
+            private string? Filepath { get; set; }
 
-        public Dictionary<string, string> langExtPairs = new Dictionary<string, string>();
+            public Dictionary<string, string> langExtPairs = new Dictionary<string, string>();
+
+            public List<string> pythonLoops = new List<string>();
+            public List<string> cSharpLoops = new List<string>();
+            public List<string> javaScriptLoops = new List<string>();
 
         private void btnAnalyze_Click(object sender, RoutedEventArgs e)
-        {
-            FillDictionary();
-
-            var chosenLang = cmbLanguage.SelectedValue.ToString();
-
-            if (txtPath.Text == string.Empty || cmbLanguage.SelectedIndex == -1)
             {
-                MessageBox.Show("Učitaj datoteku i odaberi jezik!");
-                return;
+                FillDictionaryAndLoopTypes();
+
+                var chosenLang = cmbLanguage.SelectedValue.ToString();
+
+                if (txtPath.Text == string.Empty || cmbLanguage.SelectedIndex == -1)
+                {
+                    MessageBox.Show("Učitaj datoteku i odaberi jezik!");
+                    return;
             
-            }
-            else if(langExtPairs[cmbLanguage.SelectedValue.ToString()] != Path.GetExtension(Filepath))
-            {
-                MessageBox.Show($"Odabrani jezik i tip datoteke se ne poklapaju! {langExtPairs[chosenLang]} - {Path.GetExtension(Filepath)}");
-                return;
-            }
-            else
-            {
-                if (chosenLang == "Python")
-                {
-
                 }
-                else if (chosenLang == "C#")
+                else if(langExtPairs[cmbLanguage.SelectedValue.ToString()] != Path.GetExtension(Filepath))
                 {
-
+                    MessageBox.Show($"Odabrani jezik i tip datoteke se ne poklapaju! {langExtPairs[chosenLang]} - {Path.GetExtension(Filepath)}");
+                    return;
                 }
                 else
                 {
-                    //JavaScript
+                    if (chosenLang == "Python")
+                    {
+
+                    }
+                    else if (chosenLang == "C#")
+                    {
+
+                    }
+                    else
+                    {
+                        //JavaScript
 
 
+                    }
+                }
+               
+            }
+
+            private void btnSelectDocument_Click(object sender, RoutedEventArgs e)
+            {
+                var dialog = new Microsoft.Win32.OpenFileDialog();
+                dialog.Filter = "Sve datoteke|*.*";
+
+                bool? result = dialog.ShowDialog();
+
+                if (result == true)
+                {
+                    Filepath = dialog.FileName;
+
+                    txtPath.Text += Path.GetFileName(Filepath);
+                    txtPath.Visibility = Visibility.Visible;
+                    btnRemoveFile.Visibility = Visibility.Visible;
+
+                    btnSelectDocument.IsEnabled = false;
                 }
             }
-               
-        }
 
-        private void btnSelectDocument_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-            dialog.Filter = "Sve datoteke|*.*";
-
-            bool? result = dialog.ShowDialog();
-
-            if (result == true)
+            private void btnRemoveFile_Click(object sender, RoutedEventArgs e)
             {
-                Filepath = dialog.FileName;
-
-                txtPath.Text += Path.GetFileName(Filepath);
-                txtPath.Visibility = Visibility.Visible;
-                btnRemoveFile.Visibility = Visibility.Visible;
-
-                btnSelectDocument.IsEnabled = false;
+                txtPath.Text = string.Empty;
+                txtPath.Visibility = Visibility.Hidden;
+                btnRemoveFile.Visibility = Visibility.Hidden;
+                btnSelectDocument.IsEnabled = true;
+                Filepath = null;
             }
-        }
 
-        private void btnRemoveFile_Click(object sender, RoutedEventArgs e)
-        {
-            txtPath.Text = string.Empty;
-            txtPath.Visibility = Visibility.Hidden;
-            btnRemoveFile.Visibility = Visibility.Hidden;
-            btnSelectDocument.IsEnabled = true;
-            Filepath = null;
-        }
+            private void FillDictionaryAndLoopTypes()
+            {
+                langExtPairs.Clear();
+                langExtPairs.Add("Python", ".py");
+                langExtPairs.Add("C#", ".cs");
+                langExtPairs.Add("JavaScript", ".js");
 
-        private void FillDictionary()
-        {
-            langExtPairs.Clear();
-            langExtPairs.Add("Python", ".py");
-            langExtPairs.Add("C#", ".cs");
-            langExtPairs.Add("JavaScript", ".js");
+                pythonLoops.AddRange(new string[] { "for", "while" });
+                cSharpLoops.AddRange(new string[] { "for(", "foreach(", "while(", "do" });
+                javaScriptLoops.AddRange(new string[] { "for(", "for in", "for of", "while(", "do" });
         }
     }
-}
+    }
